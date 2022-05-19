1966d4def9ca213618ac8acddd7ae9bf04deddac
jolivieri <jolivieri@pavlovmedia.com>
2022-05-18 21:57:48 -0500

    fix styled components

--- src/components/CryptoListHeader/Styled.tsx
+++ src/components/CryptoListHeader/Styled.tsx
@@ -3,18 +3,19 @@ import styled from 'styled-components/native';
 export const HeaderContainer = styled.View`
   flex-direction: row;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
   width: 100%;
+  flex: 1;
 `;
 
 export const HeaderTitle = styled.Text`
   color: #ffffff;
-  font-size: 22;
+  font-size: 22px;
   font-weight: bold;
 `;
 
 export const Avatar = styled.Image`
-  width: 50;
-  height: 50;
-  border-radius: 100;
+  width: 50px;
+  height: 50px;
+  border-radius: 50px;
 `;
